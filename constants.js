6ad2e34be48e12181a45792467cc4f1eb11ac5a4
Malli312-ui <115055178+Malli312-ui@users.noreply.github.com>
2022-10-26 23:03:03 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,28 @@
+// Options the user could type in
+const prompts = [
+  ["hi"],["college","what is college address","college address","address of college"],["chairman","Chairman"] ,["Principle","principle"],["cse hod","Cse hod"],["canteen","Canteen"],["results"],["which branch girls are beautiful"]
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello"],["ideal institute of technology vidyutnagar,kakinada,approved by aicte-new delhi&affilated by JNTU-KAKINADA"],["Dr. Paruchuri Krishna RaoM.Sc., Ph.D."],["Dr. T. Srikanth,Ph.D. – Osmania UniversityM.Tech – REC, Warangal,B.E.- Bangalore Universit"],
+  ["Mr. M.S.R.S. Prasad,Computer Science Engineering Department was established in the year 2009 with an intake of 60. The Department maintains excellence in its infrastructural and academics."],
+  ["all kinds of snack items are available in our canteen,chocolates,biscuits,dairymilk,fluse chocolate,5star chocolate,icecreams,cone icecreams,cool drinks,tata gluco ,somasa,veg puffs,egg puffs,chickenbiryani,vegbiryani,lays packets,bingomad angles,bingo"],
+  ["you need to paste this link in google https://www.results.idealtech.edu.in/"],["CSe 4th year girls think they are  beautiful but according to me 1st year cse girls like an angels"]
+]
+
+
+// Random for any other user input
+
+const alternative = [
+  "Same",
+  "Go on...",
+  "Try again",
+  "I'm listening...",
+  "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
